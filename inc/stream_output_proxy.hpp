45aa9b897de0b4eb7655810d86bf69ab737f4ed5
Greg King <gking@bender>
2014-03-10 08:53:58 -0700

    inc/stream_output_proxy.hpp:   - Changed to_string to the now implmented to_string_symmetric for the     error_output_proxy (reasoning, need a default output); Creates tigher     coupling between stream_output_proxy and symmetric errors (BAD);
    src/symmetric_error.cpp
      - Added <tuple> header (because of tuple return type for
        to_tuple_multi_errors);
      - Replaced string_stream code with calls to to_fixed_string;
      - Implemented multiple error code; [x_1 +/- e_1 +/- ... +/- e_n]
      - Implemented to_string_no_exponent; (initial case of column aligned numbers);
    
    src/stream_output_proxy.cpp
      - Added implementation of to_string_symmetric and to_latex_string_symmetric

--- inc/stream_output_proxy.hpp
+++ inc/stream_output_proxy.hpp
@@ -9,27 +9,26 @@
 
 typedef std::pair<double,double> error_type;
 
-std::string to_string(const error_type& x);
-std::string to_latex_string(const error_type& x);
+std::string to_string_symmetric(const error_type& x);
+std::string to_latex_string_symmetric(const error_type& x);
+
+std::string to_string_pdg(const error_type& x);
+std::string to_latex_string_pdg(const error_type& x);
 
 namespace numerical_output
 {
-  //these types might have to be expanded to handle either error type;
-  struct error_output_proxy;
-  struct latex_ensuremath_proxy;
-  struct latex_error_output_proxy;
-
-  struct error_output_creater {} eoc;
+  struct error_output_creater       {} eoc;
   struct latex_error_output_creater {} leoc;
-  struct latex_ensuremath_creater {} emoc;
+  struct latex_ensuremath_creater   {} emoc;
 
+  //defaults to symmetric
   struct error_output_proxy
   {
     explicit error_output_proxy(std::ostream & os) : m_os(os) {}
     friend std::ostream& operator<<(error_output_proxy const& q,
                                     error_type const& right_hand)
     {
-      return q.m_os << to_string(right_hand);
+      return q.m_os << to_string_symmetric(right_hand);
     }
     template <typename rhs>
 		friend std::ostream & operator<<(error_output_proxy const& q,
@@ -71,7 +70,6 @@ namespace numerical_output
   //called as:
   //std::cout << numerical_output::emoc << a;
 
-
 	struct latex_error_output_proxy
 	{
 		explicit latex_error_output_proxy(std::ostream & os) : m_os(os) {}
@@ -79,7 +77,7 @@ namespace numerical_output
 		friend std::ostream & operator<<(latex_error_output_proxy const& q,
 		                                 error_type const& right_hand)
 		{
-      return q.m_os << to_latex_string(right_hand);
+      return q.m_os << to_latex_string_symmetric(right_hand);
 		}
 
     friend std::ostream& operator<<(latex_error_output_proxy const& q,
@@ -88,7 +86,7 @@ namespace numerical_output
       //\def{testdef}   {\ensuremath{(1.00\pm0.50)E-1}}
       return q.m_os << "\\def\\"
                     << std::get<0>(right_hand) << "{" << emoc
-                    << to_latex_string(std::get<1>(right_hand))
+                    << to_latex_string_symmetric(std::get<1>(right_hand))
                     << "}";
     }
 
--- src/symmetric_error.cpp
+++ src/symmetric_error.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <tuple>
 
 #include "symmetric_error.hpp"
 
@@ -18,12 +19,13 @@ namespace symmetric_error
 	
     int precision = max_exponent - min_exponent + 1;
 	  double scale_value = pow(10., std::min(0, exponent_value - max_exponent));
+//	  std::stringstream value_string_stream;
+//    value_string_stream.precision(precision);
+//	  value_string_stream.setf( std::ios::fixed | std::ios::showpoint );
+//	  value_string_stream << (decimal_representation<double>(in_value) * scale_value);
+//    return value_string_stream.str();
 
-	  std::stringstream value_string_stream;
-    value_string_stream.precision(precision);
-	  value_string_stream.setf( std::ios::fixed | std::ios::showpoint );
-	  value_string_stream << (decimal_representation<double>(in_value) * scale_value);
-    return value_string_stream.str();
+    return to_fixed_string(decimal_representation<double>(in_value) * scale_value, precision);
   }
 
   std::string
@@ -47,7 +49,7 @@ namespace symmetric_error
 //	  return error_string_stream.str();
   }
 
-  std::string 
+  std::string
   to_string_value_fixed(const double in_value, const double in_error)
   {
    	int min_exponent = minimum_exponent(in_value,in_error);
@@ -69,10 +71,197 @@ namespace symmetric_error
 //	  std::stringstream error_string_stream;
 //	  error_string_stream << std::setprecision(precision) << std::setiosflags(std::ios::fixed | std::ios::showpoint);
 //	  error_string_stream << (decimal_representation<double>(error) * scale_error);		
-
 //	  return error_string_stream.str();
   }
 
-  
+
+  //Name?
+  std::tuple<std::string, std::vector<std::string>, int>
+  to_tuple_multi_errors(const double x, const std::vector<double>& in_vector_errors)
+  {
+
+    if (in_vector_errors.empty())
+    {
+      std::string in_function = __PRETTY_FUNCTION__;
+      throw std::runtime_error("exception in function " + in_function + "in " __FILE__ " " STRING__LINE__);
+    }
+
+    int exponent_value   = exponent<double>(x);
+    int current_min_expo = exponent_value;
+    int current_max_expo = exponent_value;
+
+    for (auto & error : in_vector_errors )
+    {
+      current_min_expo   = std::min(exponent<double>(error), current_min_expo);
+      current_max_expo   = std::max(exponent<double>(error), current_min_expo);
+    }
+
+    int precision = current_max_expo - current_min_expo + 1;
+    double scale_value = pow(10., std::min(0, exponent_value - current_max_expo));
+    std::string out_string = to_fixed_string<double>(decimal_representation<double>(x) * scale_value, precision);
+
+    std::vector<std::string> error_string;
+
+    for ( auto & error : in_vector_errors )
+    {
+  		int exponent_error = exponent<double>(error);
+      double scale_error = pow(10., std::min(0, exponent_error - current_max_expo));
+      std::string out_error_string = to_fixed_string<double>(decimal_representation<double>(error) * scale_error, precision);
+      error_string.push_back(out_error_string);
+    }
+    return std::tuple<std::string, std::vector<std::string>, int>(out_string, error_string, current_max_expo);
+  }
+
+  //Probably should name this better
+  std::string
+  value_to_string_multiple_errors(const double x, const std::vector<double>& vector_of_errors)
+  {
+    std::tuple<std::string, std::vector<std::string>, int> cont = to_tuple_multi_errors(x,vector_of_errors);
+    std::string out_str = std::get<0>(cont);
+    if (int expo = std::get<2>(cont))
+    {
+      out_str += "E" + boost::lexical_cast<std::string>(expo);
+    }
+    return out_str;
+  }
+
+  //Probably should name this better
+  std::vector<std::string>
+  to_string_multiple_errors_into_vector(const double x, const std::vector<double>& vector_of_errors)
+  {
+    std::tuple<std::string, std::vector<std::string>, int> cont = to_tuple_multi_errors(x,vector_of_errors);
+
+    std::vector<std::string>& error_strings = std::get<1>(cont);
+    if (int expo = std::get<2>(cont))
+    {
+      for( auto& error : error_strings)
+      {
+        error += "E" + boost::lexical_cast<std::string>(expo);
+      }
+    }
+    return error_strings;
+  }
+
+
+  //THIS might be moved to a seperate translation unit
+  std::pair<std::string,std::string>
+  to_string_no_exponent(const double value_1, const double value_2)
+  {
+    int min_expo = minimum_exponent(value_1, value_2);
+    int prec     = 1;
+    if (min_expo < 0)
+    {
+      prec = std::abs(min_expo) + 1;
+    }
+    size_t width_value_1 = to_fixed_string<double>(value_1, prec).size();
+    size_t width_value_2 = to_fixed_string<double>(value_2, prec).size();
+
+    size_t max_width     = std::max(width_value_1,width_value_2);
+
+    std::stringstream value_1_string_stream;
+    std::stringstream value_2_string_stream;
+
+    //right aligned, with fixed width (good for a column)
+    value_1_string_stream << std::setw(max_width) << std::setprecision(prec) << std::setiosflags(std::ios::right | std::ios::fixed | std::ios::showpoint);
+	    value_1_string_stream <<  value_1;
+
+    value_2_string_stream << std::setw(max_width) << std::setprecision(prec) << std::setiosflags( std::ios::right |std::ios::fixed | std::ios::showpoint);
+      value_2_string_stream << value_2;
+
+    return std::make_pair(value_1_string_stream.str(), value_2_string_stream.str());
+  }
+
+
+  std::vector<std::string>
+  to_string_no_exponent(const std::vector<double>& vec)
+  {
+    if (vec.empty())
+    {
+      std::string in_function = __PRETTY_FUNCTION__;
+      throw std::runtime_error("exception in function " + in_function + "in " __FILE__ " " STRING__LINE__);
+    }
+
+    std::vector<int> exponents;
+    //std::function<int(double)> a_func = std::bind(exponent<double>,std::placeholders::_1);
+    std::transform(vec.begin(), vec.end(),
+                   std::back_inserter(exponents),
+                   [](double in_value)
+                   {
+                     return exponent<double>(in_value);
+                   });
+
+    int min_expo = *std::min_element(exponents.begin(), exponents.end());
+    int prec = 1;
+    if (min_expo < 0)
+    {
+      prec = std::abs(min_expo) + 1;
+    }
+
+    size_t max_width = 0;
+    for (auto & in : vec)
+    {
+      max_width = std::max(max_width, to_fixed_string<double>(in,prec).size()) ;
+    }
+
+    std::vector<std::string> out_vector = {};
+    for(size_t i = 0; i < vec.size(); ++i )
+    {
+      std::stringstream value_string_stream;
+      value_string_stream << std::setw(max_width)
+                          << std::setprecision(2)
+                          << std::setiosflags(std::ios::right | std::ios::fixed | std::ios::showpoint)
+                          << vec[i];
+      out_vector.push_back( value_string_stream.str() );
+    }
+    return out_vector;
+  }
+
+
+  std::vector<std::string>
+  to_string_no_exponent(const double value_1,
+                        const double value_2,
+                        const double value_3)
+  {
+    //Original VERSION:
+    //  int min_expo_12 = minimum_exponent(value_1, value_2);
+    //  int min_expo_23 = minimum_exponent(value_2, value_3);
+    //  int min_expo = std::min(min_expo_12, min_expo_23);
+    //  int prec = 2;
+    //  if (min_expo < 0)
+    //  {
+    //    prec = std::abs(min_expo) + 1;
+    //  }
+    //  size_t width_value_1 = to_fixed_string<double>(value_1, prec).size();
+    //  size_t width_value_2 = to_fixed_string<double>(value_2, prec).size();
+    //  size_t width_value_3 = to_fixed_string<double>(value_3, prec).size();
+    //  size_t max_width     = std::max({width_value_1, width_value_2, width_value_3});
+    //  //C++11 version
+    //  //C++03
+    //  //size_t max_width   = std::max(std::max(width_value_1, width_value_2), width_value_3);
+    //  std::stringstream value_string_stream;
+    //  std::vector<std::string> out_vector;
+    //  value_string_stream << std::setw(max_width)  << std::setiosflags(std::ios::right | std::ios::fixed | std::ios::showpoint) << value_1;
+    //  out_vector.push_back(value_string_stream.str());
+    //  value_string_stream.str(std::string());
+    //  value_string_stream << std::setw(max_width) << std::setiosflags(std::ios::right | std::ios::fixed | std::ios::showpoint) << value_2;
+    //  out_vector.push_back(value_string_stream.str());
+    //  value_string_stream.str(std::string());
+    //  value_string_stream << std::setw(max_width) << std::setiosflags(std::ios::right | std::ios::fixed | std::ios::showpoint) << value_3;
+    //  out_vector.push_back(value_string_stream.str());
+    //  value_string_stream.str(std::string());
+    //  return out_vector;
+    return to_string_no_exponent({value_1,value_2,value_3});
+  }
+
+  std::vector<std::string>
+  to_string_no_exponent(const double value_1,
+                        const double value_2,
+                        const double value_3,
+                        const double value_4)
+  {
+    return to_string_no_exponent({value_1,value_2,value_3,value_4});
+  }
+
+
 }
 
